3fdfc6a2c1a9d547178514dc5467bb2c368998b8
Andrew <aschulak@gmail.com>
2015-02-19 16:23:52 -0500

    copy

--- givealittlelove/gall/mail/coupon.py
+++ givealittlelove/gall/mail/coupon.py
@@ -6,16 +6,14 @@ Please enjoy a little love from American Greetings!
 Browse to the following URL to receive a free gift!
 %s
 
-Your Happiness, Laughter and Love Ambassador,
+Your Happiness, Laughter, and Love Ambassador,
 %s
 """
 
 html_template = """
 <html>
 <head></head>
-<body bgcolor=red>
-<font color=white>
-
+<body>
 %s,
 <br/>
 Please enjoy a little love from American Greetings!
@@ -24,7 +22,7 @@ Please enjoy a little love from American Greetings!
 <a href=%s>Click here to receive a free gift.</a>
 <br/>
 <br/>
-Your Happiness, Laughter and Love Ambassador,
+Your Happiness, Laughter, and Love Ambassador,
 <br/>
 %s
 </font>
