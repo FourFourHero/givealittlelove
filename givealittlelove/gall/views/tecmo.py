9ddd6a9b4d9313c08ba392ed58f10bd7c7fa69a3
Andrew <aschulak@gmail.com>
2015-12-15 20:49:32 -0500

    tecmo

--- givealittlelove/gall/views/tecmo.py
+++ givealittlelove/gall/views/tecmo.py
@@ -20,14 +20,13 @@ def roll_team(not_team=-1):
 
 #@staff_member_required
 def vs(request, template_name):
-    response_dict = success_dict()
-    return render_template(request, response_dict, 'gall/site/' + template_name)
-
-def home(request):
     team1 = roll_team()
     team2 = roll_team(not_team=team1)
     response_dict = success_dict()
     response_dict['team1'] = team1
     response_dict['team2'] = team2
+    return render_template(request, response_dict, 'gall/site/tecmo_vs.html')
+
+def home(request):
     return render_template(request, response_dict, 'gall/site/tecmo.html')
 
