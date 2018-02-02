1d5704ea013f3dc68ce85181c5323698549f15c7
Andrew <aschulak@gmail.com>
2018-02-02 14:56:25 -0500

    fix indent error

--- givealittlelove/gall/views/tecmo.py
+++ givealittlelove/gall/views/tecmo.py
@@ -259,7 +259,7 @@ def _vs(request, teams, tiers, tier_ranking, form_action, format=None):
     logger.warn('_vs')
     team1 = _roll_team(teams)
     tiers = team1.tiers
-   idx = random.randint(0,len(tiers)-1)
+    idx = random.randint(0,len(tiers)-1)
     tier = team1.tiers[idx]
     logger.info('NEW tier: ' + str(tier))
     team2 = _roll_team(teams, tier=tier, not_team=team1.img_id)
